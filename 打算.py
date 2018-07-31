3fab99d35a4b682359c8e951e429f36c6d602bf1
Frankyzzd <905321213@qq.com>
2018-07-31 20:56:52 +0800

    增加了一个文件

---
+++ 打算.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+'''
+@license: (C) Copyright 2018-2017, Node Supply Chain Manager Corporation Limited.
+@contact: 905321213@qq.com
+@file: 打算.py
+@time: 2018/7/31 20:53
+@author:frankyzzd
+'''
+print('废材')
